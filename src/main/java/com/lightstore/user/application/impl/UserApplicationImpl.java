a820ab2a34f206008f9f16ff00b58e07d9de4f30
XuanGuang Chen <gtcxg@hotmail.com>
2025-02-07 15:14:39 +0800

    logback config

--- src/main/java/com/lightstore/user/application/impl/UserApplicationImpl.java
+++ src/main/java/com/lightstore/user/application/impl/UserApplicationImpl.java
@@ -6,12 +6,15 @@ import com.lightstore.user.domain.entity.User;
 import com.lightstore.user.domain.event.UserRegistered;
 import com.lightstore.user.domain.shared.CommandFailure;
 import io.vavr.control.Either;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserApplicationImpl implements UserApplication {
 
+    private final Logger logger = LoggerFactory.getLogger(UserApplicationImpl.class);
     private final ApplicationContext applicationContext;
 
     UserApplicationImpl(ApplicationContext applicationContext) {
@@ -21,6 +24,7 @@ public class UserApplicationImpl implements UserApplication {
     @Override
     public Either<CommandFailure, UserRegistered> registerUser(RegisterUser registerUserCommand) {
         User user = new User(this.applicationContext);
+        logger.info("register user command: {}", registerUserCommand);
         return user.handle(registerUserCommand);
     }
 }
